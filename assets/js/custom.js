6baa59851e090d4ca66dc34b99761fd1fad8a7dd
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-12-30 15:52:13 +0500

    30-12-2022) (01)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.querySelectorAll('.dropdown-menu').forEach(function(element){
-        element.addEventListener('click', function (e) {
-            e.stopPropagation();
-        });
-    })
-}); 
+new WOW().init();
+
+$(document).ready(function(){
+    $('ul li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+    
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
